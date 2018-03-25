03e5cf004349c33247644f57c93c820adb0bdcd0
Marko Arsic <marsicdev@gmail.com>
2018-03-25 22:07:23 +0200

    Add class models

---
+++ src/models/Author.js
@@ -0,0 +1,11 @@
+import Address from './Address'
+import Company from './Company'
+
+export default class Author {
+    constructor(user) {
+        this.id = user.id
+        this.fullName = user.name
+        this.address = new Address(user.address)
+        this.company = new Company(user.company)
+    }
+};
