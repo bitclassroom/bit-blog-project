ebd3fe04fa1646c788f3bd2cb86b6ccfd5a3330e
Marko Arsic <marsicdev@gmail.com>
2018-03-25 21:21:39 +0200

    Add posts page

---
+++ src/app/posts/PostsList.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import PostItem from './PostItem'
+
+const PostsList = () => {
+    return (
+        <div>
+            <PostItem />
+            <PostItem />
+            <PostItem />
+            <PostItem />
+        </div>
+    )
+}
+
+export default PostsList
